e75568402f774c311bb68a7f79e869c9ea3e01cb
Ali Elderov <info@cyberkatze.de>
2021-09-17 13:12:24 +0200

    chore: reformat "coins-travel" project

--- apps/frontend/coins-travel/src/app/app.component.ts
+++ apps/frontend/coins-travel/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'coins-travel-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
   title = 'frontend-coins-travel';
