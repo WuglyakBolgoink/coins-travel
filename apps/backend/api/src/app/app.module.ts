458a9980a5606d1b0c1ee9609ecee22de8dddc88
Ali Elderov <info@cyberkatze.de>
2021-09-20 22:20:26 +0200

    feat(nx): add typeorm for postgress

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { environment } from '../environments/environment';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...environment.connection
+    })
+  ],
+  controllers: [ AppController ],
+  providers: [ AppService ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- apps/backend/api/src/environments/environment.ts
+++ apps/backend/api/src/environments/environment.ts
@@ -1,3 +1,36 @@
-export const environment = {
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const environment: IEnvironment = {
   production: false,
+  connection: {
+    type: process.env.DB_TYPE as "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_USER_PASSWORD,
+
+    database: process.env.DB_NAME,
+
+    dropSchema: false,
+    synchronize: true,
+    logging: false
+  }
 };
+
+export interface IEnvironment {
+  production: boolean;
+  connection: TypeOrmModuleOptions;
+}
+
+export interface IEnvironmentConnection {
+  type: string;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  dropSchema: boolean;
+  synchronize: boolean;
+  logging: boolean;
+}
